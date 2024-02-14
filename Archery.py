26b1fae8e918547e077415f120c1293ddf2db06f
microgold <Batman134!!>
2024-02-13 22:19:22 -0600

    added cosine function for moving the aiming bar.

--- Archery.py
+++ Archery.py
@@ -33,23 +33,25 @@ target_radius = 50
 
 # initialize the Archer
 archer_x, archer_y = 100, height // 2
-archer_img = pygame.image.load('Images/ArcherStrip.png')  # Load your sprite here
+archer_img = pygame.image.load(
+    'Images/ArcherStrip.png')  # Load your sprite here
 background_img = pygame.image.load('Images/background.png')
 archer = Archer(archer_img, (archer_x, archer_y))
 archer_width = 150
 
-#initialize background music
+# initialize background music
 pygame.mixer.init()
 pygame.mixer.music.set_volume(0.5)
 pygame.mixer.music.load('Music/background.mp3')
 
-#initialize sound effects
+# initialize sound effects
 arrow_sound = pygame.mixer.Sound('Sounds/arrow.mp3')
 cheer_sound = pygame.mixer.Sound('Sounds/cheer.wav')
 
 
 # Initialize the arrow
-arrow_img = pygame.image.load('Images/Arrow.png')  # Load your arrow sprite here
+# Load your arrow sprite here
+arrow_img = pygame.image.load('Images/Arrow.png')
 arrow = Arrow(arrow_img, (archer_x + archer_width, archer_y + 55))
 arrow.speed = 10
 
@@ -59,38 +61,43 @@ moving_bar = MovingBar((0, height - 50), width, height)
 # Initialize the arrow markers
 hit_positions = pygame.sprite.Group()
 
-#intitialize the statistics
+# intitialize the statistics
 score_text = GameText('Score: 0', 36, BLACK, (100, 20))
-arrow_count_text = GameText(f'Arrows Left: {arrow.num_arrows}', 36, BLACK, (300, 20))
-game_over_text = GameText('Game Over', 36, RED, (width // 2 - 50, height // 2 - 20))
-play_again_text = GameText('Play Again? (y/n)', 36, GREEN, (width // 2 - 50, height // 2 + 10))
+arrow_count_text = GameText(
+    f'Arrows Left: {arrow.num_arrows}', 36, BLACK, (300, 20))
+game_over_text = GameText('Game Over', 36, RED,
+                          (width // 2 - 50, height // 2 - 20))
+play_again_text = GameText('Play Again? (y/n)', 36,
+                           GREEN, (width // 2 - 50, height // 2 + 10))
 game_over = False
 
 # initialize target
 target = Target((target_x, target_y), target_radius)
 
 # initialize Target Stand
-stand_image =  pygame.image.load('Images/stand.png')
+stand_image = pygame.image.load('Images/stand.png')
 stand_image = pygame.transform.scale(stand_image, (200, 200))
 stand = Stand(stand_image, (target_x + 5, target_y + 60), 100, 100)
 
+
 def handle_arrow_firing():
     global hit_x, hit_y, bar_center, bullseye_flag
-        
+
     arrow.reset()
     arrow.fire()
     archer.current_frame = 0
     archer.animation_counter = 0
-    
+
     # Determine the hit position
     bar_center = moving_bar.bar_x + moving_bar.bar_width / 2
     # Calculate the midpoint of the button
     button_midpoint = moving_bar.button_x + moving_bar.button_width / 2
-    bar_position_ratio = ( button_midpoint -  moving_bar.bar_x) /  moving_bar.bar_width
+    bar_position_ratio = (
+        button_midpoint - moving_bar.bar_x) / moving_bar.bar_width
     hit_position_ratio = (2 * bar_position_ratio - 1)  # -1 (left) to 1 (right)
 
     # Calculate the hit position on the target
-    hit_deviation = hit_position_ratio * target.radius # Distance from bullseye
+    hit_deviation = hit_position_ratio * target.radius  # Distance from bullseye
     target_x, target_y = target.position
     hit_x = target_x + hit_deviation
     # Adjust vertical offset based on proximity to bullseye
@@ -100,20 +107,20 @@ def handle_arrow_firing():
         max_offset = 3  # Smaller offset for closer to bullseye
     vertical_offset = random.uniform(-max_offset, max_offset)
     hit_y = target_y + vertical_offset
-    arrow.decrement_arrows() # Decrement the arrow count
-    
+    arrow.decrement_arrows()  # Decrement the arrow count
+
     # determine if we hit the bullseye and set the flag
     if abs_deviation <= target_radius * 0.1:
         bullseye_flag = True
-        
+
     print('arrow state firing ', arrow.arrow_state)
-        
+
     return hit_deviation
 
 
 def reset_game():
     global hit_positions, score, hit_x, hit_y, game_over
-    
+
     arrow.reset()
     arrow.num_arrows = 10
     archer.reset()
@@ -124,6 +131,7 @@ def reset_game():
     score = 0
     hit_x = 0
     hit_y = 0
+
 
 reset_game()
 
@@ -147,88 +155,85 @@ while running:
                 reset_game()
             if game_over and event.key == pygame.K_n:
                 running = False
-            if event.key == pygame.K_SPACE and arrow.arrow_state != ArrowStates.FIRED:
+            if event.key == pygame.K_SPACE and\
+                    arrow.arrow_state == ArrowStates.READY and\
+                    arrow.num_arrows > 0:
                 archer.current_frame = 0
                 hit_deviation = handle_arrow_firing()
                 # play arrow sound
                 arrow_sound.play()
-               
-        
-    if game_over: continue # Skip the rest of the game loop if game is over
+
+    if game_over:
+        continue  # Skip the rest of the game loop if game is over
 
     # Collision detection
     arrow_x, arrow_y = arrow.position
     target_x, target_y = target.position
     archer_x, archer_y = archer.position
-    
-    distance_to_target = ((arrow_x - target_x)**2 + (arrow_y - target_y)**2)**0.5
+
+    distance_to_target = ((arrow_x - target_x)**2 +
+                          (arrow_y - target_y)**2)**0.5
     if distance_to_target <= target.radius and arrow.arrow_state == ArrowStates.MOVING:
         arrow.arrow_state = ArrowStates.READY
-        
+
         print('arrow state ', arrow.arrow_state)
-        
-    if arrow.arrow_state == ArrowStates.READY: 
+
+    if arrow.arrow_state == ArrowStates.READY:
         pass
-    elif arrow.arrow_state == ArrowStates.FIRED: 
+    elif arrow.arrow_state == ArrowStates.FIRED:
         if archer.current_frame < archer.num_frames:
             archer.animate()
         else:
-            arrow.initial_update() 
+            arrow.initial_update()
             arrow.arrow_state = ArrowStates.MOVING
-    elif arrow.arrow_state == ArrowStates.MOVING: 
-            arrow.update()
-            if arrow_x > width or (target_x - arrow_x <= target_radius):
-                arrow.arrow_state = ArrowStates.READY                  
-                # Add the hit position to the list and calculate score
-                mark_color = target.get_mark_color(hit_deviation)
-                arrow_marker = ArrowMarker((hit_x, hit_y), mark_color)
-                hit_positions.add(arrow_marker)
-                score += target.calculate_score(hit_deviation)
-                
-                # if bullseye flag is set, cheer
-                if bullseye_flag:
-                    bullseye_flag = False
-                    cheer_sound.play()
-                
-                    # Check for game over
-                if arrow.num_arrows <= 0:
-                    game_over_text.draw(screen)   
-                    game_over = True                            
-                    play_again_text.draw(screen)
-        
-    
+    elif arrow.arrow_state == ArrowStates.MOVING:
+        arrow.update()
+        if arrow_x > width or (target_x - arrow_x <= target_radius):
+            arrow.arrow_state = ArrowStates.READY
+            # Add the hit position to the list and calculate score
+            mark_color = target.get_mark_color(hit_deviation)
+            arrow_marker = ArrowMarker((hit_x, hit_y), mark_color)
+            hit_positions.add(arrow_marker)
+            score += target.calculate_score(hit_deviation)
 
-                                
+            # if bullseye flag is set, cheer
+            if bullseye_flag:
+                bullseye_flag = False
+                cheer_sound.play()
+
+                # Check for game over
+            if arrow.num_arrows <= 0:
+                game_over_text.draw(screen)
+                game_over = True
+                play_again_text.draw(screen)
+
     # Draw the target stand
     stand.draw(screen)
-    
+
     # Draw the target
     target.draw(screen)
-    
 
-    
     # display the arrow markers where they hit on the target
     hit_positions.draw(screen)
-    
+
     # Draw the moving bar
     moving_bar.update()
     moving_bar.draw(screen)
-        
 
     # Display the score
     score_text.update_text(f'Score: {score}')
     score_text.draw(screen)
-    
+
     # Display the arrow count
     arrow_count_text.update_text(f'Arrows Left: {arrow.num_arrows}')
-    arrow_count_text.draw(screen)   
+    arrow_count_text.draw(screen)
 
     # Draw the archer
     archer.draw(screen)
 
     # Draw the arrow only if it's moving
     if arrow.arrow_state == ArrowStates.MOVING:
-        arrow.draw(screen)        
+        arrow.draw(screen)
 
     pygame.display.flip()
     clock.tick(fps)
--- Sprites.py
+++ Sprites.py
@@ -13,6 +13,7 @@ YELLOW = (255, 255, 0)
 LIGHTGRAY = (240, 240, 240)
 DARKGRAY = (100, 100, 100)
 
+
 class ArrowStates(Enum):
     READY = 1
     FIRED = 2
@@ -27,32 +28,33 @@ class Archer(pygame.sprite.Sprite):
         self.position = position
         self.num_frames = 5
         self.frame_width, self.frame_height = self.image.get_width() // 5, self.image.get_height()
-        self.frames = self.get_frames(self.image, self.frame_width, self.frame_height, self.num_frames)
+        self.frames = self.get_frames(
+            self.image, self.frame_width, self.frame_height, self.num_frames)
         self.current_frame = 0
         self.animation_speed = 5  # Adjust this value to control the speed of the animation
         self.animation_counter = 0
-    
+
     def get_frames(self, image, frame_width, frame_height, num_frames):
         frames = []
         for i in range(num_frames):
-            frame = image.subsurface(pygame.Rect(i * frame_width, 0, frame_width, frame_height))
+            frame = image.subsurface(pygame.Rect(
+                i * frame_width, 0, frame_width, frame_height))
             frames.append(frame)
         return frames
-    
+
     def reset(self):
         self.current_frame = 0
         self.animation_counter = 0
-    
-    def animate(self):        
+
+    def animate(self):
         if self.animation_counter >= self.animation_speed:
             self.current_frame += 1
             self.animation_counter = 0  # Reset counter after updating frame
         self.animation_counter += 1
-    
+
     def draw(self, screen):
         screen.blit(self.frames[self.current_frame - 1], self.position)
-    
-        
+
 
 class Arrow(pygame.sprite.Sprite):
     def __init__(self, image, position):
@@ -64,30 +66,31 @@ class Arrow(pygame.sprite.Sprite):
         self.speed = 10
         self.arrow_state = ArrowStates.READY
         self.num_arrows = 10  # Number of arrows per game
-        
+
     def initial_update(self):
         if self.arrow_state == ArrowStates.MOVING:
             self.position = self.starting_position
-    
+
     def update(self):
         if self.arrow_state == ArrowStates.MOVING:
-            (x,y) = self.position
+            (x, y) = self.position
             self.position = (x + self.speed, y)
             self.rect = self.image.get_rect(topleft=self.position)
-    
+
     def reset(self):
         arrow_state = ArrowStates.READY
         self.position = self.starting_position
-    
+
     def decrement_arrows(self):
         self.num_arrows -= 1
-        
+
     def fire(self):
         self.arrow_state = ArrowStates.FIRED
-        
+
     def draw(self, screen):
         screen.blit(self.image, self.rect)
-        
+
+
 class ArrowMarker(pygame.sprite.Sprite):
     def __init__(self, position, color):
         super().__init__()
@@ -96,7 +99,8 @@ class ArrowMarker(pygame.sprite.Sprite):
         self.rect = self.image.get_rect(center=position)
         self.color = color
         pygame.draw.circle(self.image, self.color, (2, 2), 2)
-    
+
+
 class Target(pygame.sprite.Sprite):
     def __init__(self, position, radius):
         super().__init__()
@@ -106,17 +110,18 @@ class Target(pygame.sprite.Sprite):
         self.image = pygame.Surface((radius*2, radius*2), pygame.SRCALPHA)
         self.rect = self.image.get_rect(center=position)
         for i, color in enumerate(colors):
-            pygame.draw.circle(self.image, color, (self.radius, self.radius), self.radius - i * 10)
+            pygame.draw.circle(
+                self.image, color, (self.radius, self.radius), self.radius - i * 10)
         # draw a black line around the outer edge of the target
-        pygame.draw.circle(self.image, BLACK, (self.radius, self.radius), self.radius, 1)
+        pygame.draw.circle(self.image, BLACK, (self.radius,
+                           self.radius), self.radius, 1)
         # draw a black circle outline inside the smallest circle of the target
-        pygame.draw.circle(self.image, BLACK, (self.radius, self.radius), self.radius * 0.1, 1)    
-        
+        pygame.draw.circle(self.image, BLACK, (self.radius,
+                           self.radius), self.radius * 0.1, 1)
 
-        
-    def draw(self, screen):       
-            screen.blit(self.image, self.rect)
-    
+    def draw(self, screen):
+        screen.blit(self.image, self.rect)
+
     def get_mark_color(self, hit_deviation):
         # Function to determine the color of the mark based on the hit deviation
         if abs(hit_deviation) <= self.radius * 0.2:  # Bullseye (Yellow)
@@ -129,85 +134,110 @@ class Target(pygame.sprite.Sprite):
             return LIGHTGRAY  # Black mark should be visible on blue
         else:  # Black or White ring
             return DARKGRAY  # White mark for visibility on blac
-            
+
     def calculate_score(self, deviation):
         abs_deviation = abs(deviation)
         if abs_deviation <= self.radius * 0.1:  # Bullseye
             return 10
         if abs_deviation <= self.radius * 0.2:  # Yellow
             return 9
-        elif abs_deviation <= self.radius * 0.4: # Red
+        elif abs_deviation <= self.radius * 0.4:  # Red
             return 7
-        elif abs_deviation <= self.radius * 0.6: # Blue
+        elif abs_deviation <= self.radius * 0.6:  # Blue
             return 5
-        elif abs_deviation <= self.radius: # Black
+        elif abs_deviation <= self.radius:  # Black
             return 2
         else:
             return 1  # white ring
 
-class Stand(pygame.sprite.Sprite):
-        def __init__(self, image, position, width, height):
-            super().__init__()
-            colors = [WHITE, BLACK, BLUE, RED, YELLOW]
-            self.position = position
-            self.width = width
-            self.height = height
-            self.image = image
-            self.rect = self.image.get_rect(center=position)
 
-        def draw(self, screen):       
-                screen.blit(self.image, self.rect)   
-        
+class Stand(pygame.sprite.Sprite):
+    def __init__(self, image, position, width, height):
+        super().__init__()
+        colors = [WHITE, BLACK, BLUE, RED, YELLOW]
+        self.position = position
+        self.width = width
+        self.height = height
+        self.image = image
+        self.rect = self.image.get_rect(center=position)
+
+    def draw(self, screen):
+        screen.blit(self.image, self.rect)
+
+
 class MovingBar (pygame.sprite.Sprite):
     def __init__(self, position, width, height):
         super().__init__()
         # Bar settings
-        self.bar_width,  self.bar_height = 200, 20
-        self.bar_x,  self.bar_y = (width - self.bar_width) // 2, height - 50
-        self.button_width,  self.button_height = 20, 20
-        self.button_x,  self.button_y = self.bar_x, self.bar_y
+        self.bar_width, self.bar_height = 200, 20
+        self.bar_x, self.bar_y = (width - self.bar_width) // 2, height - 50
+        self.button_width, self.button_height = 20, 20
+        self.button_x, self.button_y = self.bar_x, self.bar_y
         self.button_direction = 1
         self.button_increment = 0
-        
-        
-        self.button_position = [  
-    1.0,          2.02248126,   3.11218166,   4.31316746,
-   5.66839299,   7.21927146,   9.00526119,  11.06347135,
-  13.42829116,  16.1310463 ,  19.19968592,  22.65850337,
-  26.52789347,  30.82414874,  35.5592966 ,  40.74097919,
-  46.37237704,  52.45217726,  58.97458669,  65.92938974,
-  73.3020505 ,  81.07385797,  89.222113  ,  97.7203551 ,
- 106.53862682, 115.64377309, 124.99977248, 134.56809712,
- 144.30809765, 154.17740939, 164.13237562, 174.1284838 ,
- 184.12459198, 194.07955821, 203.94886995, 213.68887048,
- 223.25719512, 232.61319451, 241.71834078, 250.5366125 ,
- 259.0348546 , 267.18310963, 274.9549171 , 282.32757786,
- 289.28238091, 295.80479034, 301.88459056, 307.51598841,
- 312.697671  , 317.43281886, 321.72907413, 325.59846423,
- 329.05728168, 332.1259213 , 334.82867644, 337.19349625,
- 339.25170641, 341.03769614, 342.58857461, 343.94380014,
- 345.14478594, 346.23448634, 347.2569676 , 348.2569676 ]
+        self.button_position = [
+            1.0, 2.02248126, 3.11218166, 4.31316746,
+            5.66839299, 7.21927146, 9.00526119,  11.06347135,
+            13.42829116, 16.1310463, 19.19968592, 22.65850337,
+            26.52789347, 30.82414874, 35.5592966, 40.74097919,
+            46.37237704, 52.45217726, 58.97458669, 65.92938974,
+            73.3020505, 81.07385797, 89.222113,  97.7203551,
+            106.53862682, 115.64377309, 124.99977248, 134.56809712,
+            144.30809765, 154.17740939, 164.13237562, 174.1284838,
+            184.12459198, 194.07955821, 203.94886995, 213.68887048,
+            223.25719512, 232.61319451, 241.71834078, 250.5366125,
+            259.0348546, 267.18310963, 274.9549171, 282.32757786,
+            289.28238091, 295.80479034, 301.88459056, 307.51598841,
+            312.697671, 317.43281886, 321.72907413, 325.59846423,
+            329.05728168, 332.1259213, 334.82867644, 337.19349625,
+            339.25170641, 341.03769614, 342.58857461, 343.94380014,
+            345.14478594, 346.23448634, 347.2569676, 348.2569676]
 
-      
+    # update by cosine function
+    # def update(self):
+    #     self.button_increment += self.button_direction
+
+    #     cosine_period = 100
+
+    #     if (self.button_increment < 0):
+    #         self.button_increment = 0
+    #         self.button_direction = 1
+
+    #     if self.button_increment > cosine_period - 1:
+    #         self.button_increment = cosine_period - 1
+    #         self.button_direction = -1
+
+    #     self.button_x = 173.628 * \
+    #         math.cos((2*math.pi/cosine_period) * self.button_increment +
+    #                  math.pi) + 173.628 + 1
+
+    #     print("button x ", self.button_x)
+    #     self.button_x = (self.button_x *
+    #                      float(self.bar_width - self.button_width) /
+    #                      self.button_position[-1]) + self.bar_x
+
+    # update by position array
     def update(self):
+
         self.button_increment += self.button_direction
-       
-        if (self.button_increment < 0): 
+
+        if (self.button_increment < 0):
             self.button_increment = 0
             self.button_direction = 1
-            
-        if  self.button_increment > len(self.button_position) - 1: 
+
+        if self.button_increment > len(self.button_position) - 1:
             self.button_increment = len(self.button_position) - 1
             self.button_direction = -1
 
-        self.button_x = int (self.bar_x +  float(self.button_position[self.button_increment]) * float(self.bar_width) / 385.0)
-        
-    
-    
+        self.button_x = int(
+            self.bar_x + float(self.button_position[self.button_increment]) * float(self.bar_width - self.button_width) / self.button_position[-1])  # 385.0)
+
     def draw(self, screen):
-            # Draw the moving bar
-        pygame.draw.rect(screen, BLUE, (self.bar_x, self.bar_y, self.bar_width, self.bar_height))
-        pygame.draw.rect(screen, GREEN, (self.button_x, self.button_y, self.button_width, self.button_height))
+        # Draw the moving bar
+        pygame.draw.rect(screen, BLUE, (self.bar_x, self.bar_y,
+                         self.bar_width, self.bar_height))
+        pygame.draw.rect(screen, GREEN, (self.button_x,
+                         self.button_y, self.button_width, self.button_height))
 
 
 class GameText (pygame.sprite.Sprite):
@@ -220,12 +250,11 @@ class GameText (pygame.sprite.Sprite):
         self.font = pygame.font.SysFont(None, self.font_size)
         self.image = self.font.render(self.text, True, self.color)
         self.rect = self.image.get_rect(center=self.position)
-    
+
     def update_text(self, text):
         self.text = text
         self.image = self.font.render(self.text, True, self.color)
         self.rect = self.image.get_rect(center=self.position)
-    
+
     def draw(self, screen):
         screen.blit(self.image, self.rect)
-
